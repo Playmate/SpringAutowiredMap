bd16019d9523c493d03d8b3e36a70404829faa6e
Ihor <Ihor_Dmitriiev@epam.com>
2016-08-02 21:36:47 +0300

    change to AutowiredMap implementation

--- src/main/java/com/igor/autowiredmap/business/WoodlandParkZoo.java
+++ src/main/java/com/igor/autowiredmap/business/WoodlandParkZoo.java
@@ -1,10 +1,8 @@
 package com.igor.autowiredmap.business;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.igor.autowiredmap.api.AutowiredMap;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,20 +11,11 @@ import java.util.Map;
 @Component
 public class WoodlandParkZoo implements Zoo {
 
-    private final Map<LifeArea, Animal> animals = new HashMap<>();
-
-    @Autowired
-    public WoodlandParkZoo(@Qualifier("lion") Animal lion, @Qualifier("seal") Animal seal) {
-        animals.put(LifeArea.DESERT, lion);
-        animals.put(LifeArea.SEA, seal);
-    }
+    @AutowiredMap
+    private Map<LifeArea, Animal> animals;
 
     @Override
     public String say(LifeArea lifeArea) {
-        String say = animals.get(lifeArea).say();
-        if (say == null) {
-            throw new IllegalArgumentException("There is no animal in the zoo");
-        }
-        return say;
+        return animals.get(lifeArea).say();
     }
 }
